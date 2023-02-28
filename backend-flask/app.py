c8a06fb6619905e7e4c40b863b59404a874e3026
Nick Fong <nfong@helloeworld.com>
2023-02-28 20:08:14 +0000

    move middleware code to inside flask app

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -35,13 +35,16 @@ provider.add_span_processor(processor)
 # XRay -- Start Xray Recorder
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+
 
 
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+#Xray -- Midddleware layer
+XRayMiddleware(app, xray_recorder)
 
 # Honeycomb -- Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
