ebbf4ff89cf875a921fd1a586791000cc7e22782
Nick Fong <nfong@helloeworld.com>
2023-03-22 21:19:30 +0000

    TypeError: MessageGroups.run() missing 1 required positional argument: 'message_group_uuid'

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,7 +1,7 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'abrown@abrown.com','andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'byako@byko.com', 'bayko' ,'MOCK');
+  ('Andrew Brown', 'animerat+abrown@gmail.com','andrewbrown' ,'MOCK'),
+  ('Andrew Bayko', 'animerat+bayko@gmail.com', 'bayko' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
