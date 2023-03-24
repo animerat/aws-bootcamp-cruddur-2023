3741a1a5bdff464ce5c664dd7b87d8afa9909f88
Nick Fong <nfong@helloeworld.com>
2023-03-24 23:17:46 +0000

    "POST /api/messages HTTP/1.1" 401

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,7 +1,8 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Andrew Brown', 'animerat+abrown@gmail.com','andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'animerat+bayko@gmail.com', 'bayko' ,'MOCK');
+  ('Andrew Bayko', 'animerat+bayko@gmail.com', 'bayko' ,'MOCK'),
+  ('Londo Mollari', 'animerat+londo@gmail.com', 'londo', 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
