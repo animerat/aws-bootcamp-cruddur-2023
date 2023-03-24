2b5b513ca01748459205e9236454bdb1e95e2ee1
Nick Fong <nfong@helloeworld.com>
2023-03-24 23:40:17 +0000

    Still POST /api/messages HTTP/1.1

--- frontend-react-js/src/components/MessageForm.js
+++ frontend-react-js/src/components/MessageForm.js
@@ -44,7 +44,6 @@ export default function ActivityForm(props) {
         } else {
           props.setMessages(current => [...current,data]);
         }
-        props.setMessages(current => [...current,data]);
       } else {
         console.log(res)
       }
