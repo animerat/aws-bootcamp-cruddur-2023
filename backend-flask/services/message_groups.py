36fd3e950f0bbcd5f97506875d57e61c0795149f
Nick Fong <nfong@helloeworld.com>
2023-03-25 04:07:00 +0000

    Group Messages Working (Syncing issues Resolved)

--- backend-flask/services/message_groups.py
+++ backend-flask/services/message_groups.py
@@ -14,12 +14,10 @@ class MessageGroups:
     my_user_uuid = db.query_value(sql,{
       'cognito_user_id': cognito_user_id
     })
-
     print(f"UUID: {my_user_uuid}")
-
     ddb = Ddb.client()
     data = Ddb.list_message_groups(ddb, my_user_uuid)
     print("list_message_groups:",data)
-    
+
     model['data'] = data
     return model
